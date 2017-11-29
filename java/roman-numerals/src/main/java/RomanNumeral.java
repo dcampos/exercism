3edab7101c45f6e39d246a5a587d17b260e4d663
Darlan de Campos <darlanpedro@gmail.com>
2017-11-29 10:59:34 -0200

    Add more solutions

---
+++ java/roman-numerals/src/main/java/RomanNumeral.java
@@ -0,0 +1,28 @@
+import static java.util.Collections.nCopies;
+
+class RomanNumeral {
+
+    private final int decimal;
+
+    private static int[] numbers =
+        {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static String[] symbols =
+        {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    RomanNumeral(int decimal) {
+        this.decimal = decimal;
+    }
+
+    String getRomanNumeral() {
+        int value = decimal;
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < numbers.length; i++) {
+            int repeat = value / numbers[i];
+            value -= numbers[i] * repeat;
+            result.append(String.join("", nCopies(repeat, symbols[i])));
+        }
+
+        return result.toString();
+    }
+}
